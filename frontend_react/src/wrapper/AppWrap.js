300f5ec8ae337e509a77efa55f02856637ef7b6e
Jordan Kimsey <jordankimsey@gmail.com>
2023-01-15 10:07:42 -0500

    Added dynamic year -JK

--- frontend_react/src/wrapper/AppWrap.js
+++ frontend_react/src/wrapper/AppWrap.js
@@ -3,13 +3,14 @@ import { NavigationDots, SocialMedia } from '../components';
 
 const AppWrap = (Component, idName, classNames) =>
   function HOC() {
+    const currentYear = new Date().getFullYear();
     return (
       <div id={idName} className={`app__container ${classNames}`}>
         <SocialMedia />
         <div className='app__wrapper app__flex'>
           <Component />
           <div className='copyright'>
-            <p className='p-text'>@2023 JORDAN</p>
+            <p className='p-text'>@{currentYear} JORDAN</p>
             <p className='p-text'>All rights reserved</p>
           </div>
         </div>
